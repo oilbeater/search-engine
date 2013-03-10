8856c01558cc485b508fae593543058aad8972d1
Oilbeater <liumengxinfly@gmail.com>
2013-03-10 20:58:02 +0800

    add url trim function

---
+++ TextPareser.py
@@ -0,0 +1,16 @@
+import re
+def trimUrl(findUrl , currentUrl):
+    findUrl = findUrl.strip()
+    if findUrl.startswith('/'):
+        findUrl = currentUrl + findUrl
+    if findUrl.endswith('/'):
+        findUrl = findUrl[:-1]
+    if not (findUrl.startswith('http') or findUrl.startswith('https')):
+        return ''
+   # filename = re.search(r'/([^/]*)$',findUrl)
+   # suffix = re.search(r'\.(.*)',filename.group(1))
+   # if suffix!=None and suffix.group(1) not in ['html','htm','']:
+   #     return ''
+    return findUrl
+findUrl = '   http://oilbeater.com/  '
+print trimUrl(findUrl,'http://oilbeater.com')
