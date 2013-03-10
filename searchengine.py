e60c02e14e919bf6594cd2083609bebb17c8c2f5
Oilbeater <liumengxinfly@gmail.com>
2013-03-10 20:55:55 +0800

    The crawler gather the urls

---
+++ searchengine.py
@@ -0,0 +1,27 @@
+﻿import urllib2
+import datetime
+from TextPareser import trimUrl
+import re
+starttime = datetime.datetime.now()
+
+url_seeds = set(["http://oilbeater.com"])
+url_visited = set([])
+urls = set([])
+while len(url_seeds) > 0 and len(urls) < 50:
+    source = url_seeds.pop()
+    try:
+        url_visited.add(source)
+        f = urllib2.urlopen(source,None,15)
+        text =  f.read()
+    except (urllib2.HTTPError,urllib2.URLError):
+        text = ''
+    finds = re.findall(r'href=["\']([^"\']*)["\'][^>]*>(?:[^<]*)<',text,re.I)
+    for x in finds:
+        if  x not in url_visited:
+            trimedUrl = trimUrl(x,source)
+            if trimedUrl:
+                urls.add(trimedUrl)
+                url_seeds.add(trimedUrl)
+
+endtime = datetime.datetime.now()
+interval=(endtime - starttime).seconds
