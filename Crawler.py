d99c72763e869769b8257cc588c7d9bc29b824d9
Oilbeater <liumengxinfly@gmail.com>
2013-03-15 10:52:09 +0800

    change to insite search fix a bug --trim Url first before insert into url_visited

--- Crawler.py
+++ Crawler.py
@@ -1,6 +1,7 @@
 ﻿#"-*- coding: utf-8 -*-" 
 print u"中无奈"
 import urllib2
+import socket
 import datetime
 from TextPareser import *
 import re
@@ -9,22 +10,26 @@ starttime = datetime.datetime.now()
 url_seeds = set(["http://oilbeater.com"])
 url_visited = set([])
 urls = set([])
+url2file = {"http://oilbeater.com":1}
+globalindex = 1
+
 key2url = {'oilbeater':set(['oilbeater.com'])}
-while len(url_seeds) > 0 and len(urls) < 50:
+while len(url_seeds) > 0 and len(urls) < 1000:
     source = url_seeds.pop()
     try:
         url_visited.add(source)
-        f = urllib2.urlopen(source,None,15)
+        f = urllib2.urlopen(source,None,3)
         text =  unicode(f.read(),'utf-8')
-    except (urllib2.HTTPError,urllib2.URLError):
+    except (urllib2.HTTPError,urllib2.URLError,socket.timeout):
         text = ''
     finds = re.findall(r'href=["\']([^"\']*)["\'][^>]*>([^<]*)<',text,re.I)
     for x in finds:
-        if  x[0] not in url_visited:
-            trimedUrl = trimUrl(x[0],source)
-            if trimedUrl:
+        trimedUrl = trimUrl(x[0],"http://oilbeater.com")
+        if  trimedUrl not in url_visited:
+            if trimedUrl and trimedUrl.startswith("http://oilbeater.com") and trimedUrl.find("/css") == -1:
                 urls.add(trimedUrl)
-                url_seeds.add(trimedUrl)
+                if trimedUrl not in url_seeds:
+                    url_seeds.add(trimedUrl)
                 trimedKey = trimKey(x[1])
                 if trimedKey:
                     if not key2url.has_key(trimedKey):
